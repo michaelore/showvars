d975884e20a7e419659ac9391841a18a70a96787
Michael Ore <selbram1993@gmail.com>
2013-08-22 23:01:50 -0500

    Adding 'ban' method

--- showvars.py
+++ showvars.py
@@ -3,6 +3,10 @@ from pprint import pprint
 class ShowVars(object):
     def __init__(self, lcls = {}):
         self.hideall(lcls)
+        self.blacklist = set()
+
+    def ban(self, varname):
+        self.blacklist.add(varname)
 
     def hide(self, k, v):
         self.hashdict[k] = hash(repr(v))
@@ -13,7 +17,9 @@ class ShowVars(object):
     def showvars(self, lcls):
         showdict = {}
         for k, v in lcls.items():
-            if k not in self.hashdict or self.hashdict[k] != hash(repr(v)):
+            if k not in self.blacklist and \
+              (k not in self.hashdict or \
+               self.hashdict[k] != hash(repr(v))):
                 showdict[k] = v
                 self.hide(k, v)
         pprint(showdict)
