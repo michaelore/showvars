d15fbb9163dcff869d6368b21418c9db0044049a
Michael Ore <selbram1993@gmail.com>
2013-08-22 23:09:21 -0500

    Fleshing out example.py

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import showvars
 
 sv = showvars.ShowVars(locals())
+sv.ban('sv')
+sv.ban('t')
 
 def distance(t):
     v = 5
@@ -13,4 +15,19 @@ for t in range(10):
     dist = distance(t)
     data.append(dist)
 
+print('Call #1:')
+sv.showvars(locals())
+
+for t in range(10):
+    data[t] = 2*data[t]
+
+toomuchdata = range(10000)
+sv.hide('toomuchdata', toomuchdata)
+
+print('Call #2:')
+sv.showvars(locals())
+
+lessdata = range(20)
+
+print('Call #3:')
 sv.showvars(locals())
